ad43acb5b9e66a6feddf5916fa2509f909e16e5c
Benjie Gillam <benjie@jemjie.com>
2019-03-18 12:36:11 +0000

    Watch mode

--- scripts/schema_pg.sql
+++ scripts/schema_pg.sql
@@ -1,6 +1,7 @@
 DROP SCHEMA IF EXISTS shakespeare CASCADE;
 CREATE SCHEMA shakespeare;
 SET search_path TO shakespeare, public;
+
 CREATE TABLE work
   ( work_id    VARCHAR(32) PRIMARY KEY,
     title     VARCHAR(32) NOT NULL,
@@ -13,41 +14,43 @@ CREATE TABLE work
     total_paragraphs INTEGER NOT NULL
   );
 
-\copy work FROM '../opensourceshakespeare/Works.txt' WITH (FORMAT csv, QUOTE '~')
+\copy work FROM 'opensourceshakespeare/Works.txt' WITH (FORMAT csv, QUOTE '~')
+
 
 CREATE TABLE character 
-  ( char_id      VARCHAR(32) PRIMARY KEY,
-    char_name    VARCHAR(64) NOT NULL,
+  ( character_id      VARCHAR(32) PRIMARY KEY,
+    name    VARCHAR(64) NOT NULL,
     abbrev      VARCHAR(32),
-    works       VARCHAR(256) NOT NULL,
+    work_ids_string       VARCHAR(256) NOT NULL,
     description VARCHAR(2056),
     speech_count INTEGER NOT NULL
   );
 
-\copy character FROM '../opensourceshakespeare/Characters.txt' WITH (FORMAT csv, QUOTE '~')
+\copy character FROM 'opensourceshakespeare/Characters.txt' WITH (FORMAT csv, QUOTE '~')
 
 CREATE TABLE character_work
-  ( char_id     VARCHAR(32) NOT NULL REFERENCES character (char_id),
+  ( character_id     VARCHAR(32) NOT NULL REFERENCES character (character_id),
     work_id     VARCHAR(32) NOT NULL REFERENCES work (work_id),
-    PRIMARY KEY (char_id, work_id)
+    PRIMARY KEY (character_id, work_id)
   );
 
-INSERT INTO character_work (char_id, work_id)
-SELECT char_id,
-       regexp_split_to_table(works, ',')
+INSERT INTO character_work (character_id, work_id)
+SELECT character_id,
+       regexp_split_to_table(work_ids_string, ',')
 FROM   character;
 
-ALTER TABLE character DROP COLUMN works;
+ALTER TABLE character DROP COLUMN work_ids_string;
 
 CREATE TABLE chapter 
   ( work_id         VARCHAR(32) NOT NULL REFERENCES work (work_id),
     chapter_id      INTEGER PRIMARY KEY,
-    section        INTEGER NOT NULL,
-    chapter        INTEGER NOT NULL,
-    description    VARCHAR(256) NOT NULL
+    section_number        INTEGER NOT NULL,
+    chapter_number        INTEGER NOT NULL,
+    description    VARCHAR(256) NOT NULL,
+    unique (work_id, section_number, chapter_number)
   );
 
-\copy chapter FROM '../opensourceshakespeare/Chapters.txt' WITH (FORMAT csv, QUOTE '~')
+\copy chapter FROM 'opensourceshakespeare/Chapters.txt' WITH (FORMAT csv, QUOTE '~')
 
 UPDATE chapter SET description = '' WHERE description LIKE '---%';
 UPDATE chapter SET description = REPLACE(description, '&#8217;','''');
@@ -55,18 +58,20 @@ UPDATE chapter SET description = REPLACE(description, '&#8217;','''');
 CREATE TABLE paragraph
   ( work_id         VARCHAR(32) NOT NULL REFERENCES work (work_id), paragraph_id    INTEGER PRIMARY KEY NOT NULL,
     paragraph_num   INTEGER NOT NULL,
-    char_id         VARCHAR(32) NOT NULL REFERENCES character (char_id),
+    character_id         VARCHAR(32) NOT NULL REFERENCES character (character_id),
     plain_text      TEXT NOT NULL,
     phonetic_text   TEXT NOT NULL,
     stem_text       TEXT NOT NULL,
     paragraph_type  VARCHAR(1) NOT NULL,
-    section        INTEGER NOT NULL,
-    chapter        INTEGER NOT NULL,
+    section_number        INTEGER NOT NULL,
+    chapter_number        INTEGER NOT NULL,
     char_count      INTEGER NOT NULL,
-    word_count      INTEGER NOT NULL
+    word_count      INTEGER NOT NULL,
+    constraint paragraph_chapter_fkey foreign key (work_id, section_number, chapter_number) references chapter (work_id, section_number, chapter_number)
   );
 
-\copy paragraph FROM '../opensourceshakespeare/Paragraphs.txt' WITH (FORMAT csv, QUOTE '~')
+
+\copy paragraph FROM 'opensourceshakespeare/Paragraphs.txt' WITH (FORMAT csv, QUOTE '~')
 
 CREATE TABLE wordform
   ( word_form_id     INTEGER PRIMARY KEY,
@@ -76,5 +81,7 @@ CREATE TABLE wordform
     occurences     INTEGER NOT NULL
   );
 
-\copy wordform FROM '../opensourceshakespeare/WordForms.txt' WITH (FORMAT csv, QUOTE '~')
+\copy wordform FROM 'opensourceshakespeare/WordForms.txt' WITH (FORMAT csv, QUOTE '~')
 
+-- PostGraphile customisations
+comment on constraint paragraph_chapter_fkey on paragraph is E'@fieldName chapter\n@foreignFieldName paragraphs';
