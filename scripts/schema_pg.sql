5120ae1af3edaacbe6c06dbf7e1e5d0afc004765
Benjie Gillam <benjie@jemjie.com>
2019-03-18 12:41:32 +0000

    PK first

--- scripts/schema_pg.sql
+++ scripts/schema_pg.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA shakespeare;
 SET search_path TO shakespeare, public;
 
 CREATE TABLE work
-  ( work_id    VARCHAR(32) PRIMARY KEY,
+  ( id    VARCHAR(32) PRIMARY KEY,
     title     VARCHAR(32) NOT NULL,
     long_title VARCHAR(64) NOT NULL,
     year      INTEGER NOT NULL,
@@ -18,7 +18,7 @@ CREATE TABLE work
 
 
 CREATE TABLE character 
-  ( character_id      VARCHAR(32) PRIMARY KEY,
+  ( id      VARCHAR(32) PRIMARY KEY,
     name    VARCHAR(64) NOT NULL,
     abbrev      VARCHAR(32),
     work_ids_string       VARCHAR(256) NOT NULL,
@@ -29,36 +29,37 @@ CREATE TABLE character
 \copy character FROM 'opensourceshakespeare/Characters.txt' WITH (FORMAT csv, QUOTE '~')
 
 CREATE TABLE character_work
-  ( character_id     VARCHAR(32) NOT NULL REFERENCES character (character_id),
-    work_id     VARCHAR(32) NOT NULL REFERENCES work (work_id),
+  ( character_id     VARCHAR(32) NOT NULL REFERENCES character,
+    work_id     VARCHAR(32) NOT NULL REFERENCES work,
     PRIMARY KEY (character_id, work_id)
   );
 
 INSERT INTO character_work (character_id, work_id)
-SELECT character_id,
+SELECT id,
        regexp_split_to_table(work_ids_string, ',')
 FROM   character;
 
 ALTER TABLE character DROP COLUMN work_ids_string;
 
 CREATE TABLE chapter 
-  ( work_id         VARCHAR(32) NOT NULL REFERENCES work (work_id),
-    chapter_id      INTEGER PRIMARY KEY,
+  ( id      INTEGER PRIMARY KEY,
+    work_id         VARCHAR(32) NOT NULL REFERENCES work,
     section_number        INTEGER NOT NULL,
     chapter_number        INTEGER NOT NULL,
     description    VARCHAR(256) NOT NULL,
     unique (work_id, section_number, chapter_number)
   );
 
-\copy chapter FROM 'opensourceshakespeare/Chapters.txt' WITH (FORMAT csv, QUOTE '~')
+\copy chapter (work_id, id, section_number, chapter_number, description) FROM 'opensourceshakespeare/Chapters.txt' WITH (FORMAT csv, QUOTE '~')
 
 UPDATE chapter SET description = '' WHERE description LIKE '---%';
 UPDATE chapter SET description = REPLACE(description, '&#8217;','''');
 
 CREATE TABLE paragraph
-  ( work_id         VARCHAR(32) NOT NULL REFERENCES work (work_id), paragraph_id    INTEGER PRIMARY KEY NOT NULL,
+  ( id    INTEGER PRIMARY KEY NOT NULL,
+    work_id         VARCHAR(32) NOT NULL REFERENCES work,
     paragraph_num   INTEGER NOT NULL,
-    character_id         VARCHAR(32) NOT NULL REFERENCES character (character_id),
+    character_id         VARCHAR(32) NOT NULL REFERENCES character,
     plain_text      TEXT NOT NULL,
     phonetic_text   TEXT NOT NULL,
     stem_text       TEXT NOT NULL,
@@ -71,10 +72,10 @@ CREATE TABLE paragraph
   );
 
 
-\copy paragraph FROM 'opensourceshakespeare/Paragraphs.txt' WITH (FORMAT csv, QUOTE '~')
+\copy paragraph (work_id, id, paragraph_num, character_id, plain_text, phonetic_text, stem_text, paragraph_type, section_number, chapter_number, char_count, word_count) FROM 'opensourceshakespeare/Paragraphs.txt' WITH (FORMAT csv, QUOTE '~')
 
 CREATE TABLE wordform
-  ( word_form_id     INTEGER PRIMARY KEY,
+  ( id     INTEGER PRIMARY KEY,
     plain_text      VARCHAR(64) NOT NULL,
     phonetic_text   VARCHAR(64) NOT NULL,
     stem_text       VARCHAR(64) NOT NULL,
